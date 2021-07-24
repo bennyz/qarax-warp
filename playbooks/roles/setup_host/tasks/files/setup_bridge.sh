3c47a336e51389a88669ad69c32e7f74a7d1d2c0
Benny Zlotnik <bzlotnik@protonmail.com>
2021-07-24 13:52:27 +0300

    fix ansible stuff

--- playbooks/roles/setup_host/tasks/files/setup_bridge.sh
+++ playbooks/roles/setup_host/tasks/files/setup_bridge.sh
@@ -1,32 +1,16 @@
-- name: set ipv4 forwarding
-  sysctl:
-    name: net.ipv4.conf.all.forwarding
-    value: 1
-    state: present
+#!/bin/bash
 
-- name: proxy arp
-  sysctl:
-    name: net.ipv4.conf.all.proxy_arp
-    value: 1
-    state: present
+INTERFACE=$(ip route get 8.8.8.8 | awk '{ print $5; exit }')
+IP=$(hostname -I)
+IP_WITH_CIDR=$(ip addr show dev $INTERFACE | grep "inet " | awk '{print $2}')
+ROUTE=$(ip route list dev $INTERFACE | grep -v default | cut -f 1 -d ' ')
+DEFAULT_ROUTE=$(ip route list dev $INTERFACE | grep default | awk '{print $3}')
 
-- name: disable ipv6
-  sysctl:
-    name: net.ipv6.conf.all.disable_ipv6
-    value: 1
-    state: present
-
-- name: copy setup_bridge.sh to host
-  copy:
-    src: ./files/setup_bridge.sh
-    dest: setup_bridge.sh
-    mode: u=rwx
-
-- name: check if bridge present
-  shell: ip addr show dev fcbridge | grep fcbridge
-  register: fcbridge
-  ignore_errors: true
-
-- name: run bridge setup
-  shell: ./setup_bridge.sh
-  when: fcbridge.rc == 1
+ip link add fcbridge type bridge
+ip link set fcbridge up
+ip addr flush dev $INTERFACE
+ip link set $INTERFACE master fcbridge
+ip addr add $IP_WITH_CIDR brd + dev fcbridge
+ip route add default via $DEFAULT_ROUTE
+ip route add $ROUTE dev fcbridge proto kernel scope link src $IP
+exit 0
