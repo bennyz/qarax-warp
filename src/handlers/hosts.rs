feab16edf6724a751c3fae3f617ba74e47d4d434
Benny Zlotnik <bzlotnik@protonmail.com>
2021-07-24 18:03:27 +0300

    WIP host installation

--- src/handlers/hosts.rs
+++ src/handlers/hosts.rs
@@ -1,11 +1,12 @@
 use crate::env::Environment;
 
 use super::*;
-use models::hosts::HostError;
-use models::hosts::NewHost;
+use models::hosts as host_model;
+use models::hosts::{HostError, NewHost, Status};
+use uuid::Uuid;
 
 pub async fn list(env: Environment) -> Result<impl warp::Reply, warp::Rejection> {
-    let hosts = models::hosts::list(env.db()).await;
+    let hosts = host_model::list(env.db()).await;
     match hosts {
         Ok(hosts) => Ok(ApiResponse::Success(SuccessResponse {
             code: StatusCode::OK,
@@ -13,30 +14,45 @@ pub async fn list(env: Environment) -> Result<impl warp::Reply, warp::Rejection>
         })),
         Err(e) => Ok(ApiResponse::Error(ErrorResponse {
             code: StatusCode::BAD_REQUEST,
-            error: HostError::ErrorList(e).to_string(),
+            error: HostError::ErrorList(e.to_string()),
         })),
     }
 }
 
-pub async fn add(
-    host: NewHost,
-    env: Environment,
-) -> Result<impl warp::Reply, warp::Rejection> {
-    let host_id = models::hosts::add(env.db(), &host).await;
-    if let Ok(id) = host_id {
-        let response = response::Builder::new()
-            .status(StatusCode::CREATED)
-            .body(json!({ "response": id }).to_string())
-            .unwrap();
-        Ok(response)
-    } else {
-        let response = response::Builder::new()
-            .status(StatusCode::BAD_REQUEST)
-            .body(
-                json!({"error": HostError::NameAlreadyExists(host.name.to_owned()).to_string()})
-                    .to_string(),
-            )
-            .unwrap();
-        Ok(response)
+pub async fn add(host: NewHost, env: Environment) -> Result<impl warp::Reply, warp::Rejection> {
+    let host_id = host_model::add(env.db(), &host).await;
+    match host_id {
+        Ok(host_id) => Ok(ApiResponse::Success(SuccessResponse {
+            code: StatusCode::OK,
+            data: host_id,
+        })),
+        Err(e) => Ok(ApiResponse::Error(ErrorResponse {
+            code: StatusCode::BAD_REQUEST,
+            error: HostError::ErrorList(e.to_string()),
+        })),
     }
+}
+
+pub async fn install(host_id: Uuid, env: Environment) -> Result<impl warp::Reply, warp::Rejection> {
+    let host = host_model::by_id(env.db(), host_id).await;
+    if host.is_err() {
+        return Ok(ApiResponse::Error(ErrorResponse {
+            code: StatusCode::NOT_FOUND,
+            error: HostError::HostNotFound(host_id).to_string(),
+        }));
+    }
+
+    host_model::update_status(env.db(), &host.unwrap(), Status::Installing)
+        .await
+        .map_err(|e| {
+            return Ok*ApiResponse::Error(ErrorResponse {
+                code: StatusCode::NOT_FOUND,
+                error: HostError::HostUpdateFailed,
+            });
+        });
+
+    Ok(ApiResponse::Success(SuccessResponse {
+        code: StatusCode::ACCEPTED,
+        data: "Installing",
+    }))
 }
