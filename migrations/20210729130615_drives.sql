951dca5c6056424b59bebeec2409b3c6ee5a08af
Benny Zlotnik <bzlotnik@protonmail.com>
2021-07-29 16:12:58 +0300

    create migrations for storage and vms

---
+++ migrations/20210729130615_drives.sql
@@ -0,0 +1,9 @@
+-- Add migration script here
+CREATE TABLE drives (
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL,
+    status VARCHAR(18) NOT NULL,
+    readonly boolean NOT NULL,
+    rootfs boolean NOT NULL,
+    storage_id UUID REFERENCES storage(id) NOT NULL
+);
